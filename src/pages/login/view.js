08554a40eca6b5688ab08439185e14283b2c4f41
tomvaneygen <tom.van.eygen@student.howest.be>
2017-11-02 15:20:18 +0100

    Importing styled components in Loginview

--- src/pages/login/view.js
+++ src/pages/login/view.js
@@ -1,9 +1,53 @@
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { submit } from './actions';
-import { loginStyle } from '../../constants/styles';
+import { loginStyle, colors } from '../../constants/styles';
+import styled from 'styled-components';
 
 const logo = require('./smackLogo.svg');
+
+const Container = styled.div`
+align-items: center;
+background-color: ${colors.darkPurple};
+display: flex;
+height: 100%;
+justify-content: center;
+position: absolute;
+width: 100%;
+`;
+
+const Form = styled.form`
+height: 250px;
+width: 21px;
+`;
+
+const Logo = styled.img`
+margin-bottom: 29px;
+`;
+
+const Input = styled.input`
+background-color: ${colors.white};
+border: solid 1px ${colors.grey};
+border-radius: 6px;
+box-shadow: inset 0 1px 3px 0 ${colors.lightGrey};
+color: ${colors.silver};
+font-family: Rubik-Italic;
+font-size: 16px;
+height: 40px;
+margin-bottom: 11px;
+text-align: center;
+width: 212px;
+`;
+
+const Button = styled.button`
+background-color: ${colors.darkPurple};
+border: solid 1px ${colors.lightGrey};
+border-radius: 6px;
+color: ${colors.white};
+font-family: Rubik-Light;
+height: 40px;
+width: 212px;
+`;
 
 @reduxForm({
   form: 'login',
@@ -28,19 +72,18 @@ export default class LoginForm extends Component {
   submit (values, dispatch) {
     dispatch(submit(values));
   }
-
   render () {
     const styles = loginStyle;
     const { fields: { username }, handleSubmit, submitting } = this.props;
 
     return (
-      <div style={styles.container}>
-        <form style={styles.form} onSubmit={handleSubmit(this.submit)}>
-          <img src={logo} style={styles.logo} />
-          <input {...username} autoComplete='off' placeholder='Your name' style={styles.input} type='text' />
-          <button disabled={submitting} style={styles.button} type='submit'>Join</button>
-        </form>
-      </div>
+      <Container>
+        <Form onSubmit={handleSubmit(this.submit)}>
+          <Logo src={logo} />
+          <Input {...username} autoComplete='off' placeholder='Your name' type='text' />
+          <Button disabled={submitting} type='submit'>Join</Button>
+        </Form>
+      </Container>
     );
   }
 }
